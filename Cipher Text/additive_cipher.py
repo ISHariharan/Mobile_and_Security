37fe9b8111e475c005c5039ce2a4e6f153aeab83
Hariharan.I.S <112647530+ISHariharan@users.noreply.github.com>
2024-08-05 11:03:29 +0100

    additive_cipher_changes

--- Cipher Text/additive_cipher.py
+++ Cipher Text/additive_cipher.py
@@ -2,25 +2,30 @@ str = input("Enter a word : ")
 key = int(input("Enter a key : "))
 length = len(str)
 empty = ""
+str = str.lower()
 
 
 for i in str:
-    temp = (ord(i) + key) % 97
-    temp = temp % 26
-    temp = temp + 97
-    empty += chr(temp)
+    if(i.isalpha()):
+        temp = (ord(i) + key) % 97
+        temp = temp % 26
+        temp = temp + 97
+        empty += chr(temp)
+    else: empty += chr(ord(i) + key)
 
 print("Encypted string : ", empty)
 
-str = ""
+result = ""
 for i in empty :
-    if(ord(i) - key < 97):
-        div = (ord(i) - key) + 26
-    else:
-        div = ord(i) - key
-    temp = (div) % 97
-    temp = temp % 26
-    temp = temp + 97
-    str += chr(temp)
+    if(i.isalpha()) :
+        if(ord(i) - key < 97):
+            div = (ord(i) - key) + 26
+        else:
+            div = ord(i) - key
+        temp = (div) % 97
+        temp = temp % 26
+        temp = temp + 97
+        result += chr(temp)
+    else : result += chr(ord(i) - key)
     
-print("Decrypted string : ", str)
+print("Decrypted string : ", result)
